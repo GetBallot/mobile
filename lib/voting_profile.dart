bf5aa7566c2299d0f4c964b0265dd1c22c43e68c
Chiu-Ki Chan <chiuki@users.noreply.github.com>
2018-06-27 18:58:57 -0600

    Implement voting profile as a list with items of various types

--- lib/voting_profile.dart
+++ lib/voting_profile.dart
@@ -33,7 +33,7 @@ class VotingProfile extends StatelessWidget {
     );
   }
 
-  Widget _createAddressHeader() => StreamBuilder(
+  Widget _createAddressValue() => StreamBuilder(
       stream: User.getAddressRef(firebaseUser).snapshots(),
       builder: (context, snapshot) {
         if (snapshot.hasData) {
@@ -53,119 +53,124 @@ class VotingProfile extends StatelessWidget {
         return Center(child: CircularProgressIndicator());
       });
 
-  Widget _createVoteInfoBody(context, DocumentSnapshot doc) {
-    final election =
-        doc.exists && doc.data != null ? doc.data['election'] : null;
-    final contests =
-        doc.exists && doc.data != null ? doc.data['contests'] : null;
-    final votingLocations =
-        doc.exists && doc.data != null ? doc.data['votingLocations'] : null;
-    final loading = !doc.exists;
+  List _createListItems(context, DocumentSnapshot doc) {
+    final items = [];
 
-    var headerCount = 2; // address header
-    if (loading) {
-      headerCount += 1; // loading indicator
+    items.add({'_listItemType': 'addressHeader'});
+    items.add({'_listItemType': 'addressValue'});
+
+    if (!(doc.exists)) {
+      items.add({'_listItemType': 'loading'});
+      return items;
     }
 
-    final votingLocationsCount = _getVotingLocationsCount(votingLocations);
-    final contestsCount = _getContestsCount(contests);
+    final election =
+        doc.exists && doc.data != null ? doc.data['election'] : null;
+
+    List votingLocations =
+        doc.exists && doc.data != null ? doc.data['votingLocations'] : null;
+
+    if (votingLocations != null && votingLocations.length > 0) {
+      items.add({'_listItemType': 'votingLocationHeader'});
+      final location = votingLocations[0];
+      location['_listItemType'] = 'votingLocation';
+      items.add(location);
+    }
+
+    final List contests =
+        doc.exists && doc.data != null ? doc.data['contests'] : null;
+
+    if (contests != null) {
+      items.add({
+        '_listItemType': 'header',
+        'text': BallotLocalizations.of(context).contestsHeader
+      });
+      for (int i = 0; i < contests.length; ++i) {
+        final contest = contests[i];
+        contest['_listItemType'] = 'contest';
+        contest['electionId'] = election['id'];
+        contest['contestIndex'] = i;
+        items.add(contest);
+      }
+    }
+
+    return items;
+  }
+
+  Widget _createVoteInfoBody(context, DocumentSnapshot doc) {
     final theme = Theme.of(context);
+    final items = _createListItems(context, doc);
     return ListView.builder(
-      itemCount: headerCount + votingLocationsCount + contestsCount,
-      itemBuilder: (context, index) {
-        if (index == 0) {
-          return getHeader(theme,
-              text: BallotLocalizations.of(context).votingAddressLabel,
-              trailing: BallotLocalizations.of(context).divisionsTitle,
-              onTap: () {
-            Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => DivisionsPage(firebaseUser),
-                ));
-          });
-        }
-        if (index == 1) {
-          return _createAddressHeader();
-        }
-        if (index == 2) {
-          if (loading) {
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          final Map item = items[index];
+          final String type = item['_listItemType'];
+
+          if (type == 'loading') {
             return ListTile(
                 leading: CircularProgressIndicator(),
                 title: Text(BallotLocalizations.of(context).loading));
           }
-          if (votingLocationsCount > 0) {
-            return _getPollingStationItem(
-                context,
-                BallotLocalizations.of(context).votingLocationTitle,
-                votingLocations,
-                index - headerCount);
-          } else {
-            return _getContestItem(context, election, contests,
-                index - headerCount - votingLocationsCount);
+
+          if (type == 'addressHeader') {
+            return getHeader(theme,
+                text: BallotLocalizations.of(context).votingAddressLabel,
+                trailing: BallotLocalizations.of(context).divisionsTitle,
+                onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => DivisionsPage(firebaseUser),
+                  ));
+            });
           }
-        }
-        if (index - headerCount < votingLocationsCount) {
-          return _getPollingStationItem(
-              context,
-              BallotLocalizations.of(context).votingLocationTitle,
-              votingLocations,
-              index - headerCount);
-        } else {
-          return _getContestItem(context, election, contests,
-              index - headerCount - votingLocationsCount);
-        }
-      },
-    );
-  }
 
-  int _getVotingLocationsCount(items) =>
-      (items == null || items.length == 0) ? 0 : 2;
-  Widget _getPollingStationItem(context, header, stations, index) {
-    final theme = Theme.of(context);
-    if (index == 0) {
-      return getHeader(theme,
-          text: header,
-          trailing: BallotLocalizations.of(context).all, onTap: () {
-        Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => PollingStationsPage(stations),
-            ));
-      });
-    }
-    final stationIndex = index - 1;
-    final station = stations[stationIndex];
+          if (type == 'addressValue') {
+            return _createAddressValue();
+          }
 
-    return PollingStationPage.getAddressHeader(context, station);
-  }
+          if (type == 'votingLocationHeader') {
+            List votingLocations = doc.exists && doc.data != null
+                ? doc.data['votingLocations']
+                : null;
+            return getHeader(theme,
+                text: BallotLocalizations.of(context).votingLocationTitle,
+                trailing: BallotLocalizations.of(context).all, onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => PollingStationsPage(votingLocations),
+                  ));
+            });
+          }
 
-  int _getContestsCount(contests) =>
-      (contests == null) ? 0 : contests.length + 1;
-  Widget _getContestItem(context, election, contests, index) {
-    final theme = Theme.of(context);
-    if (index == 0) {
-      return getHeader(theme,
-          text: BallotLocalizations.of(context).contestsHeader);
-    }
+          if (type == 'votingLocation') {
+            return PollingStationPage.getAddressHeader(context, item);
+          }
 
-    if (contests == null) {
-      return ListTile(
-          title: Text(BallotLocalizations.of(context).nullContests));
-    }
+          if (type == 'header') {
+            return getHeader(theme, text: item['text']);
+          }
 
-    final contestIndex = index - 1;
-    final contest = contests[contestIndex];
-    return ListTile(
-        title: Text(contest['name']),
-        onTap: () {
-          final ref = User
-              .getRef(firebaseUser)
-              .collection('elections')
-              .document('upcoming');
-          Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) => ContestPage(
-                    firebaseUser: firebaseUser,
-                    ref: ref,
-                    electionId: election['id'],
-                    contestIndex: contestIndex),
-              ));
+          if (type == 'text') {
+            return ListTile(title: item['text']);
+          }
+
+          if (type == 'contest') {
+            return ListTile(
+                title: Text(item['name']),
+                onTap: () {
+                  final ref = User
+                      .getRef(firebaseUser)
+                      .collection('elections')
+                      .document('upcoming');
+                  Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => ContestPage(
+                            firebaseUser: firebaseUser,
+                            ref: ref,
+                            electionId: item['electionId'],
+                            contestIndex: item['contestIndex']),
+                      ));
+                });
+          }
+
+          return Container();
         });
   }
 
